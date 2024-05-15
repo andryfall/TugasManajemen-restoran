e175054c41aa84657b674977102f0b8c7e713d49
Muhammad Rafa Eka Pramoedya <91171858+TheR4V3@users.noreply.github.com>
2024-05-15 11:56:33 +0700

    update restoran

--- src/main/java/com/mycompany/restoran/Restoran.java
+++ src/main/java/com/mycompany/restoran/Restoran.java
@@ -49,7 +49,7 @@ public class Restoran {
 
             switch (choice) {
                 case 1:
-                    System.out.println("Daftar Menu:");
+                    System.out.println("Daftar Menuu:");
                     ArrayList<Menu> menuList = admin.getMenuList();
                     for (Menu menu : menuList) {
                         System.out.println(menu.getNamaMenu() + "\tRp. " + menu.getHarga());
